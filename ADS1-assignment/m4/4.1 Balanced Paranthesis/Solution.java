318727d439fdaed6d48683663df03d8e3c0006c3
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-27 17:16:02 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
@@ -3,88 +3,94 @@ import java.util.Scanner;
  * { item_description }
  */
 final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		String input;
-		Scanner scan = new Scanner(System.in);
-		int size = scan.nextInt();
-		for (int i = 0; i < size; i++) {
-			BalancedParenthesis obj = new BalancedParenthesis();
-			input = scan.next();
-			obj.add(input);
-			System.out.println(obj.check());
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String input;
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        for (int i = 0; i < size; i++) {
+            BalancedParenthesis obj = new BalancedParenthesis();
+            input = scan.next();
+            obj.add(input);
+            System.out.println(obj.check());
 
-		}
+        }
 
-	}
+    }
 }
+/**.
+ * Class for balanced parenthesis.
+ */
 class BalancedParenthesis {
-	/**.
-	 * { var_description }
-	 */
-	private int size = 0;
-	/**
-	 * { item_description }
-	 */
-	private String[] stack;
-	/**.
-	 * { var_description }
-	 */
-	private String[] elements;
-	/**.
-	 * { function_description }
-	 *
-	 * @param      input  The input
-	 */
-	public void add(final String input) {
-		int len = input.length();
-		stack = new String[len];
-		elements = input.split("");
-		for (int i = 0; i < elements.length; i++) {
-			// char a = elements.charAt(i);
-			if (elements[i].equals(
-			            "(") || elements[i].equals(
-			            "[") || elements[i].equals("{")) {
-				stack[size++] = elements[i];
-			} else if (elements[i].equals(
-			               ")") || elements[i].equals(
-			               "]") || elements[i].equals("}")) {
-				if (size != 0) {
-					if (elements[i].equals(")") && (
-					            stack[size - 1].equals("("))) {
-						size--;
+    /**.
+     * { var_description }
+     */
+    private int size = 0;
+    /**.
+     * { item_description }
+     */
+    private String[] stack;
+    /**.
+     * { var_description }
+     */
+    private String[] elements;
+    /**.
+     * { function_description }
+     *
+     * @param      input  The input
+     */
+    public void add(final String input) {
+        int len = input.length();
+        stack = new String[len];
+        elements = input.split("");
+        for (int i = 0; i < elements.length; i++) {
+            // char a = elements.charAt(i);
+            if (elements[i].equals(
+                        "(") || elements[i].equals(
+                        "[") || elements[i].equals("{")) {
+                stack[size++] = elements[i];
+            } else if (elements[i].equals(
+                           ")") || elements[i].equals(
+                           "]") || elements[i].equals("}")) {
+                if (size != 0) {
+                    if (elements[i].equals(")") && (
+                                stack[size - 1].equals(
+                                    "("))) {
+                        size--;
 
-					} else if (elements[i].equals("]") && (
-					               stack[size - 1].equals("["))) {
-						size--;
-					} else if (elements[i].equals("}") && (
-					               stack[size - 1].equals("{"))) {
-						size--;
-					}
-				}
-			}
-		}
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public String check() {
-		if (size == 0) {
-			return "YES";
-		}
-		return "NO";
-	}
+                    } else if (elements[i].equals("]") && (
+                                   stack[size - 1].equals(
+                                       "["))) {
+                        size--;
+                    } else if (elements[i].equals("}") && (
+                                   stack[size - 1].equals(
+                                       "{"))) {
+                        size--;
+                    }
+                }
+            }
+        }
+    }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public String check() {
+        if (size == 0) {
+            return "YES";
+        }
+        return "NO";
+    }
 
 }
