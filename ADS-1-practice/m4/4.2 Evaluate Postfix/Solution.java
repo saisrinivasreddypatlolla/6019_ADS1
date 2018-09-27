86e91f25eb2e0cb7f3375e3de89840b131501f75
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-27 20:01:34 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-practice/m4/4.2 Evaluate Postfix/Solution.java
+++ ADS-1-practice/m4/4.2 Evaluate Postfix/Solution.java
@@ -3,10 +3,25 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	final static int twenty = 20;
-	private static int[] stack = new int[twenty];
+	/**.
+	 * this is for size of array
+	 */
+	final static int TWENTY = 20;
+	/**.
+	 * stack to store numbers
+	 */
+	private static int[] stack = new int[TWENTY];
+	/**.
+	 * size of the given set of numbers
+	 */
 	private static int size = 0;
-	public static void main(String[] args) {
+	/**.
+	 * the main function is for the input and perform
+	 * operations on them.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String[] input = scan.nextLine().split(" ");
 		for (int i = 0; i < input.length; i++) {
@@ -20,7 +35,12 @@ final class Solution {
 		}
 		System.out.println(stack[0]);
 	}
-	public static void operation(String operator) {
+	/**.
+	 * this method is used to perform operation.
+	 *
+	 * @param      operator  The operator
+	 */
+	public static void operation(final String operator) {
 		if (operator.equals("+")) {
 			stack[size - 2] = stack[size - 2] + stack[size - 1];
 			size--;
