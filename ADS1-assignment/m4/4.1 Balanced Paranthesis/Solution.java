3f3891deaa9c5f285955a2d788bfa44aff5061ef
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-27 16:21:19 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		String input;
+		Scanner scan = new Scanner(System.in);
+		int size = scan.nextInt();
+		BalancedParenthesis obj = new BalancedParenthesis();
+		for (int i = 0; i < size; i++) {
+			input = scan.next();
+			obj.add(input);
+			System.out.println(obj.check());
+
+		}
+
+	}
+}
+class BalancedParenthesis {
+	int size = 0;
+	String[] stack, elements;
+	public void add(String input) {
+		int len = input.length();
+		stack = new String[len];
+		elements = input.split("");
+		for (int i = 0; i < elements.length; i++) {
+			// char a = elements.charAt(i);
+			if (elements[i].equals("(") || elements[i].equals("[") || elements[i].equals("{")) {
+				stack[size++] = elements[i];
+			} else if (elements[i].equals(")") || elements[i].equals("]") || elements[i].equals("}")) {
+				if(size==0){
+					return;
+				}
+				if (stack[size - 1].equals("(") && elements[i].equals(")")) {
+					size--;
+				} else if (stack[size - 1].equals("[") && elements[i].equals("]")) {
+					size--;
+				} else if (stack[size - 1].equals("[") && elements[i].equals("]")) {
+					size--;
+				} else {
+					return;
+				}
+			}
+		}
+	}
+	public String check(){
+		if(size-1==0){
+			return "YES";
+		}
+		return "NO";
+	}
+
+}
