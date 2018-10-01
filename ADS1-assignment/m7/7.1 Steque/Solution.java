f187c5640339b7a382985068aaf2200ff12b3dc7
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-01 15:11:34 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS1-assignment/m7/7.1 Steque/Solution.java
+++ ADS1-assignment/m7/7.1 Steque/Solution.java
@@ -2,46 +2,47 @@ import java.util.Scanner;
 /**.
  * Class for solution.
  */
-class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	Solution() {
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    Solution() {
 
-	}
-	/**.
-	 * the main function to perform operations.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		LinkedList obj = new LinkedList();
-		int values = Integer.parseInt(scan.nextLine());
-		while (scan.hasNext()) {
-			String line = scan.nextLine();
-			String[] tokens = line.split(" ");
-			if (line.length() == 0) {
-				obj.setSize(0);
-				System.out.println();
-			} else {
-				switch (tokens[0]) {
-				case "push":
-					obj.addStart(Integer.parseInt(tokens[1]));
-					obj.print();
-					break;
-				case "pop":
-					obj.removeStart();
-					obj.print();
-					break;
-				case "enqueue":
-					obj.addEnd(Integer.parseInt(tokens[1]));
-					obj.print();
-					break;
-				default:
-					break;
-				}
-			}
-		}
-	}
+    }
+    /**.
+     * the main function to perform operations.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        LinkedList obj = new LinkedList();
+        int values = Integer.parseInt(scan.nextLine());
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(" ");
+            if (line.length() == 0) {
+                obj.setSize(0);
+                System.out.println();
+            } else {
+                switch (tokens[0]) {
+                case "push":
+                    obj.addStart(Integer.parseInt(
+                                     tokens[1]));
+                    obj.print();
+                    break;
+                case "pop":
+                    obj.removeStart();
+                    obj.print();
+                    break;
+                case "enqueue":
+                    obj.addEnd(Integer.parseInt(tokens[1]));
+                    obj.print();
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
 }
