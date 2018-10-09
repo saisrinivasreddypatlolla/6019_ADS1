350fd7d6c06788f236e59b7dea378ebd112aeb0d
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-09 21:48:56 +0530

    "ads1_m14_a1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1-assignment/m14/14.1 Dynamic Median Finding/Solution.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+class MedianFinding {
+	MinPQ<Double> minObj;
+	MaxPQ<Double> maxObj;
+	MedianFinding() {
+		minObj = new MinPQ<Double>();
+		maxObj = new MaxPQ<Double>();
+	}
+	public void minInsert(Double item) {
+		minObj.insert(item);
+	}
+	public void maxInsert(Double item) {
+		maxObj.insert(item);
+	}
+	public Double getMin() {
+		return minObj.min();
+	}
+	public Double getMax(){
+		return maxObj.max();
+	}
+	public Double delMin(){
+		return minObj.delMin();
+	}
+	public Double delMax(){
+		return maxObj.delMax();
+	}
+	public int getMaxSize(){
+		return maxObj.size();
+	}
+	public int getMinSize(){
+		return minObj.size();
+	}
+}
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		MedianFinding obj = new MedianFinding();
+		int lines = Integer.parseInt(scan.nextLine());
+		Double value = Double.parseDouble(scan.nextLine());
+		Double median = (Double) value;
+		obj.minInsert(value);
+		System.out.println(median);
+		for(int i = 1;i<lines;i++){
+			value = Double.parseDouble(scan.nextLine());
+			if(value>=median){
+				obj.minInsert(value);
+			} else{
+				obj.maxInsert(value);
+			}
+			if(obj.getMinSize()-obj.getMaxSize()>1){
+				obj.maxInsert(obj.delMin());
+			}
+			if(obj.getMaxSize()-obj.getMinSize()>1){
+				obj.minInsert(obj.delMax());
+			}
+			if(Math.abs(obj.getMaxSize()-obj.getMinSize())==1){
+				if(obj.getMinSize()>obj.getMinSize()){
+					median = (Double) obj.getMin();
+					System.out.println(median);
+				} else{
+					median = (Double) obj.getMax();
+					System.out.println(median);
+				}
+			}
+			if(obj.getMinSize()==obj.getMaxSize()){
+				median = (obj.getMin()+obj.getMax())/2.0;
+				System.out.println(median);
+			}
+		}
+	}
+}
