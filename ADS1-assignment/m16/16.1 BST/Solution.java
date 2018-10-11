d9f3cfdfb490284fe2aff006d3579879d84afc61
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-11 18:37:24 +0530

    "ads1_m16_a1: 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1-assignment/m16/16.1 BST/Solution.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+class Book {
+	String name;
+	String author;
+	double price;
+	Book(String bookName, String authorName, double cost) {
+		this.name = bookName;
+		this.author = authorName;
+		this.price = cost;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public int compareTo(Book obj) {
+		return this.getName().compareTo(obj.getName());
+	}
+}
+class BinarySearchT<Key extends Comparable<Key>, Values> {
+	Node root;
+	class Node {
+		Book key;
+		Values value;
+		Node right;
+		Node left;
+		Node(Book key1, Values val) {
+			this.key = key1;
+			this.value = val;
+		}
+
+	}
+	public void put(Book key, Values value) {
+		root = put(root, key, value);
+	}
+	public Node put(Node node, Book key, Values value) {
+		if (node == null) {
+			return new Node(key, value);
+		}
+		int cmp = key.compareTo(node.key);
+		if (cmp > 0) {
+			node.left = put(node.left, key, value);
+		} else if (cmp < 0) {
+			node.right = put(node.right, key, value);
+		} else {
+			node.value = value;
+		}
+		return node;
+	}
+	public Values get(Book key) {
+        return get(root, key);
+    }
+
+    private Values get(Node x, Book key) {
+        // if (key == null) throw new IllegalArgumentException("calls get() with a null key");
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else              return x.value;
+    }
+}
+class Solution {
+	private Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		BinarySearchT obj = new BinarySearchT();
+		while (scan.hasNext()) {
+			String[] tokens = scan.nextLine().split(",");
+			switch (tokens[0]) {
+			case "put":
+				obj.put(new Book(tokens[1], tokens[2], Double.parseDouble(
+				                     tokens[3])), Integer.parseInt(tokens[4]));
+				break;
+			case "get":
+				System.out.println(obj.get(new Book(
+				                               tokens[1], tokens[2],
+				                               Double.parseDouble(
+				                                   tokens[3]))));
+			}
+		}
+	}
+}
