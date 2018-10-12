688965799f28f19270ae0c8bcfe62385b796d2c6
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-12 13:27:44 +0530

    "taxi_cab_numbers: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1-assignment/m17/TaxiCabNumbers/CubeSum.java
@@ -0,0 +1,24 @@
+public class CubeSum implements Comparable<CubeSum> {
+    private final int sum;
+    private final int i;
+    private final int j;
+
+    public CubeSum(int i, int j) {
+        this.sum = i*i*i + j*j*j;
+        this.i = i;
+        this.j = j;
+    }
+
+    public int compareTo(CubeSum that) {
+        if (this.sum < that.sum) return -1;
+        if (this.sum > that.sum) return +1;
+        return 0;
+    }
+
+    public String toString() {
+        return sum + " = " + i + "^3" + " + " + j + "^3";
+    }
+
+
+    
+}
---
+++ ADS1-assignment/m17/TaxiCabNumbers/Solution.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+class CubeSum implements Comparable<CubeSum> {
+    private final int sum;
+    private final int i;
+    private final int j;
+
+    public CubeSum(int i, int j) {
+        this.sum = i*i*i + j*j*j;
+        this.i = i;
+        this.j = j;
+    }
+    public int getSum(){
+    	return this.sum;
+    }
+    public int getFirst(){
+    	return this.i;
+    }
+    public int getSecond(){
+    	return this.j;
+    }
+
+    public int compareTo(CubeSum that) {
+        if (this.sum < that.sum) return -1;
+        if (this.sum > that.sum) return +1;
+        return 0;
+    }
+
+    public String toString() {
+        return sum + " = " + i + "^3" + " + " + j + "^3";
+    }
+
+
+    
+}
+class Solution{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String[] tokens = scan.nextLine().split(" ");
+		int count = 1;
+        int N = Integer.parseInt(tokens[0]);
+        int M = Integer.parseInt(tokens[1]);
+        int n = 600;
+        int temp = -1;
+
+        // initialize priority queue
+        MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
+        for (int i = 0; i <= n; i++) {
+            pq.insert(new CubeSum(i, i));
+        }
+
+        // find smallest sum, print it out, and update
+        while (!pq.isEmpty()) {
+            CubeSum s = pq.delMin();
+            if(temp == s.getSum()){
+            	count++;
+            	if(count==M){
+            		N--;
+            		if(N==0){
+            			System.out.println(s.getSum());
+            		}
+            	}
+            }
+            if (s.getSecond() < n)
+                pq.insert(new CubeSum(s.getFirst(), s.getSecond() + 1));
+        }
+    }
+
+}
