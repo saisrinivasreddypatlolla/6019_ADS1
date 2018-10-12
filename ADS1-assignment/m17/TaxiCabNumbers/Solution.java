b9778f368026579549530e0293da8a46d6fdee78
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-12 14:48:21 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m17/TaxiCabNumbers/Solution.java
+++ ADS1-assignment/m17/TaxiCabNumbers/Solution.java
@@ -1,68 +1,73 @@
 import java.util.Scanner;
 class CubeSum implements Comparable<CubeSum> {
-    private final int sum;
-    private final int i;
-    private final int j;
+	private final int sum;
+	private final int i;
+	private final int j;
 
-    public CubeSum(int i, int j) {
-        this.sum = i*i*i + j*j*j;
-        this.i = i;
-        this.j = j;
-    }
-    public int getSum(){
-    	return this.sum;
-    }
-    public int getFirst(){
-    	return this.i;
-    }
-    public int getSecond(){
-    	return this.j;
-    }
+	public CubeSum(int i, int j) {
+		this.sum = i * i * i + j * j * j;
+		this.i = i;
+		this.j = j;
+	}
+	public int getSum() {
+		return this.sum;
+	}
+	public int getFirst() {
+		return this.i;
+	}
+	public int getSecond() {
+		return this.j;
+	}
 
-    public int compareTo(CubeSum that) {
-        if (this.sum < that.sum) return -1;
-        if (this.sum > that.sum) return +1;
-        return 0;
-    }
+	public int compareTo(CubeSum that) {
+		if (this.sum < that.sum) return -1;
+		if (this.sum > that.sum) return +1;
+		return 0;
+	}
 
-    public String toString() {
-        return sum + " = " + i + "^3" + " + " + j + "^3";
-    }
+	public String toString() {
+		return sum + " = " + i + "^3" + " + " + j + "^3";
+	}
 
 
-    
+
 }
-class Solution{
+class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String[] tokens = scan.nextLine().split(" ");
 		int count = 0;
-        int N = Integer.parseInt(tokens[0]);
-        int M = Integer.parseInt(tokens[1]);
-        int n = 600;
-        int temp = -1;
+		int N = Integer.parseInt(tokens[0]);
+		int M = Integer.parseInt(tokens[1]);
+		int n = 600;
+		int temp = -1;
 
-        // initialize priority queue
-        MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
-        for (int i = 0; i <= n; i++) {
-            pq.insert(new CubeSum(i, i));
-        }
+		// initialize priority queue
+		MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
+		for (int i = 0; i <= n; i++) {
+			pq.insert(new CubeSum(i, i));
+		}
 
-        // find smallest sum, print it out, and update
-        while (!pq.isEmpty()) {
-            CubeSum s = pq.delMin();
-            if(temp == s.getSum()){
-            	count++;
-            	if(count==M){
-            		N--;
-            		if(N==0){
-            			System.out.println(s.getSum());
-            		}
-            	}
-            }
-            if (s.getSecond() < n)
-                pq.insert(new CubeSum(s.getFirst(), s.getSecond() + 1));
-        }
-    }
+		// find smallest sum, print it out, and update
+		while (!pq.isEmpty()) {
+			CubeSum s = pq.delMin();
+			if (temp == s.getSum()) {
+				count++;
+			} else {
+				count = 0;
+			}
+			if (count == M - 1) {
+				N--;
+				if (N == 0) {
+					System.out.println(s.getSum());
+					break;
+				}
+			}
+
+			temp = s.getSum();
+			if (s.getSecond() < n)
+				pq.insert(new CubeSum(s.getFirst(), s.getSecond() + 1));
+		}
+	}
 
 }
