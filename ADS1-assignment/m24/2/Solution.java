0f061f26e167ae86dc542758a5b12c0c9b2d5ec4
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-27 13:01:37 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m24/2/Solution.java
+++ ADS1-assignment/m24/2/Solution.java
@@ -3,10 +3,9 @@ class StudentDetails {
 	Integer rollNumber;
 	String name;
 	Double totalMarks;
-	StudentDetails(Integer roll, String name1, Double marks) {
+	StudentDetails(Integer roll, String name1) {
 		this.rollNumber = roll;
 		this.name = name1;
-		this.totalMarks = marks;
 	}
 	public Integer getRoll() {
 		return this.rollNumber;
@@ -20,12 +19,12 @@ class StudentDetails {
 }
 class Solution {
 	public static void main(String[] args) {
-		BST<Double, String> obj = new BST<Double, String>();
+		BST<Double, StudentDetails> obj = new BST<Double, StudentDetails>();
 		Scanner scan = new Scanner(System.in);
 		int inputs = Integer.parseInt(scan.nextLine());
 		for (int i = 0; i < inputs; i++) {
 			String[] tokens = scan.nextLine().split(",");
-			obj.put(Double.parseDouble(tokens[2]), tokens[1]);
+			obj.put(Double.parseDouble(tokens[2]), new StudentDetails(Integer.parseInt(tokens[0]),tokens[1]));
 		}
 		int values = Integer.parseInt(scan.nextLine());
 		for (int j = 0; j < values; j++) {
@@ -33,7 +32,7 @@ class Solution {
 			if (tokens1[0].equals("BE")) {
 				String[] keys = obj.keys(Double.parseDouble(tokens1[1]),Double.parseDouble(tokens1[2])).toString().split(" ");
 				for(int i = 0;i<keys.length;i++){
-					System.out.println(obj.get(Double.parseDouble(keys[i])));
+					System.out.println(obj.get(Double.parseDouble(keys[i])).getName());
 				}
 				System.out.println(Arrays.toString(keys));
 
