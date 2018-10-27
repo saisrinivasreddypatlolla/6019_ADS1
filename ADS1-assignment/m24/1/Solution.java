ee61880edff54235bea7f9e6d0147807f8297543
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-27 11:17:36 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1-assignment/m24/1/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+final class Solution {
+	private Solution() {
+
+	}
+	public static void main(String[] args) {
+		LinearProbingHashST<Integer, String> st =
+		    new LinearProbingHashST<Integer, String>();
+		Scanner scan = new Scanner(System.in);
+		int inputs = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < inputs; i++) {
+			String[] tokens = scan.nextLine().split(",");
+			String s = tokens[1] + "," + tokens[2];
+			st.put(Integer.parseInt(tokens[0]), s);
+		}
+		int values = Integer.parseInt(scan.nextLine());
+		for (int j = 0; j < values; j++) {
+			String[] tokens1 = scan.nextLine().split(" ");
+			String s = st.get(Integer.parseInt(tokens1[1]));
+			if (s != null) {
+				String[] tokens2 = s.split(",");
+				if (Integer.parseInt(tokens1[2]) == 1) {
+					System.out.println(tokens2[0]);
+				} else {
+					System.out.println(Double.parseDouble(tokens2[1]));
+				}
+			} 
+		}
+	}
+
+}
