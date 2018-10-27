38c02f570a1de4ab8d503e61c80a2359a08c5aa1
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-27 13:06:27 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m24/2/Solution.java
+++ ADS1-assignment/m24/2/Solution.java
@@ -24,17 +24,16 @@ class Solution {
 		int inputs = Integer.parseInt(scan.nextLine());
 		for (int i = 0; i < inputs; i++) {
 			String[] tokens = scan.nextLine().split(",");
-			obj.put(Double.parseDouble(tokens[2]), new StudentDetails(Integer.parseInt(tokens[0]),tokens[1]));
+			obj.put(Double.parseDouble(tokens[2]), new StudentDetails(Integer.parseInt(tokens[0]), tokens[1]));
 		}
 		int values = Integer.parseInt(scan.nextLine());
 		for (int j = 0; j < values; j++) {
-			String[] tokens1 = scan.nextLine().split(",");
+			String[] tokens1 = scan.nextLine().split(" ");
 			if (tokens1[0].equals("BE")) {
-				String[] keys = obj.keys(Double.parseDouble(tokens1[1]),Double.parseDouble(tokens1[2])).toString().split(" ");
-				for(int i = 0;i<keys.length;i++){
+				String[] keys = obj.keys(Double.parseDouble(tokens1[1]), Double.parseDouble(tokens1[2])).toString().split(" ");
+				for (int i = 0; i < keys.length; i++) {
 					System.out.println(obj.get(Double.parseDouble(keys[i])).getName());
 				}
-				System.out.println(Arrays.toString(keys));
 
 
 			}
